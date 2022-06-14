b5f1b5cb5fd8f04a890e7cbf3e5946510fc697d5
Chinami Miyashita <106375669+Natty0404@users.noreply.github.com>
2022-06-14 20:38:54 +0900

    [Finish] 4-19

--- meshiterro/config/routes.rb
+++ meshiterro/config/routes.rb
@@ -3,11 +3,11 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as: 'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-   resources :post_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
   end
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy
-  ]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
 
 end
